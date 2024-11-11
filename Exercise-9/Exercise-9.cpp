384f800a76584f73b736c493682510b33ad24edd
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 02:58:02 +0000

    Add project files.

---
+++ Exercise-9/Exercise-9.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    
+    
+    scanf_s("%d", &a);
+    
+    scanf_s("%d", &b);
+
+    
+    if (a <= 0 || b <= 0 || a >= b) {
+        printf("Please ensure that a and b are positive and a < b.\n");
+      
+    }
+
+    
+    printf("The numbers ending in 3 in the interval [%d, %d] are:\n", a, b);
+    for (int i = a; i <= b; i++) {
+        if (i % 10 == 3) {
+            printf("%d", i);
+        }
+    }
+
+    
+}
+
+
